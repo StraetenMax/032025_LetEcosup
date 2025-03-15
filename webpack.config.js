e659f7c98b56b0774f16ee7cbb796da44d99f6ff
philippe <maxstraeten@yahoo.com>
2025-03-15 06:18:01 +0100

    mise en place du webpack pour pug vers mjml vers html

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,60 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { exec } = require("child_process");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./src/templates/email.html",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
     module: {
-        rules: [],
+        rules: [
+            {
+                test: /\.pug$/,
+                use: ["pug-loader"],
+            },
+            {
+                test: /\.mjml$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: { name: "[name].html"},
+                    },
+                    {
+                        loader: "mjml-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+        ],
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: "./src/templates/email.pug",
+            filename: "email.html",
+        }),
+    ],
     devServer: {
         static: path.join(__dirname, "dist"),
         compress: true,
-        port: 8080,
+        port: 3000,
+        hot: true,
+        onAfterSetupMiddleware: function () {
+            console.log("MailHog devrait être en cours d'exécution sur http://localhost:8025");
+        }
     },
 };
+
+//Générer l'émail MJML en HTML après la compilation
+exec("npx mjml src/templates/index.mjml -o dist/index.html", (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Erreur MJML : ${stderr}`);
+        return;
+    }
+    console.log("✅ Email MJML généré !");
+});
