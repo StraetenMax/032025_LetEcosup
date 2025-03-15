51476a7216691f40e56d5b073700b929e37fbd2b
philippe <maxstraeten@yahoo.com>
2025-03-15 05:37:23 +0100

    création du webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [],
+    },
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8080,
+    },
+};
