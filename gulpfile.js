d181bfc050d0d4b90b82866fc580e1e39962fcb7
philippe <maxstraeten@yahoo.com>
2025-03-16 06:53:07 +0100

    premiers travaux avec nunjucks et mjml

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-const gulp = require('gulp');
-const pug = require('gulp-pug');
-const mjml = require('gulp-mjml');
-const nunjucks = require('gulp-nunjucks');
-const data = require('gulp-data');
-const rename = require('gulp-rename');
-const beautify = require('gulp-beautify');
-const plumber = require('gulp-plumber');
-const cache = require('gulp-cache');
-const htmlmin = require('gulp-htmlmin');
-const notify = require('gulp-notify');
-const imagemin = require('gulp-imagemin');
-const w3cHtmlValidator = require('gulp-w3c-html-validator');
+import gulp from 'gulp';
+//import { compile as nunjucksCompile } from 'gulp-nunjucks';
+import nunjucksRender from 'gulp-nunjucks-render';
+import data from 'gulp-data';
+import mjml from 'gulp-mjml';
+import beautify from 'gulp-beautify';
+import rename from 'gulp-rename';
+//import plumber from 'gulp-plumber';
+//import notify from 'gulp-notify';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // Chemin des fichiers sources et de sortie
 const srcPath = 'src/';
@@ -18,24 +17,25 @@ const distPath = 'dist/';
 
 // Fonction pour obtenir les données à injecter dans les templates Nunjucks
 function getData() {
-  return require('./data.json');
+  return require('./data/data.json');
 }
 
-// Tâche pour compiler Pug en templates intermédiaires
-function compilePugToIntermediate() {
-  return gulp.src(`${srcPath}**/*.pug`)
-    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-    .pipe(cache(pug({ pretty: true })))
-    .pipe(rename({ extname: '.njk' }))
-    .pipe(gulp.dest(`${srcPath}intermediate/`));
-}
-
-// Tâche pour compiler les templates intermédiaires Nunjucks en MJML
+// Tâche pour compiler Nunjucks en MJML
 function compileNunjucksToMjml() {
-  return gulp.src(`${srcPath}intermediate/**/*.njk`)
-    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+  return gulp.src(`${srcPath}**/*.njk`)
+    //.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
     .pipe(data(getData))
-    .pipe(nunjucks.compile())
+    .pipe(nunjucksRender({
+        path: ['src/templates', 'src/partials'],
+        filters: {
+          uppercase: function(str) {
+            return str.toUpperCase();
+          }
+        },
+        globals: {
+          siteName: 'Mon Site'
+        }
+      }))
     .pipe(rename({ extname: '.mjml' }))
     .pipe(gulp.dest(`${srcPath}mjml/`));
 }
@@ -43,33 +43,22 @@ function compileNunjucksToMjml() {
 // Tâche pour compiler MJML en HTML
 function compileMjmlToHtml() {
   return gulp.src(`${srcPath}mjml/**/*.mjml`)
-    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+    //.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
     .pipe(mjml())
     .pipe(beautify.html({ indent_size: 2 }))
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(w3cHtmlValidator())
     .pipe(gulp.dest(distPath));
 }
 
-// Tâche pour optimiser les images
-function optimizeImages() {
-  return gulp.src(`${srcPath}images/**/*`)
-    .pipe(imagemin())
-    .pipe(gulp.dest(`${distPath}images/`));
-}
-
 // Tâche par défaut
-const build = gulp.series(compilePugToIntermediate, compileNunjucksToMjml, compileMjmlToHtml, optimizeImages);
+const build = gulp.series(compileNunjucksToMjml, compileMjmlToHtml);
 
 // Surveillance des fichiers pour les changements
 function watchFiles() {
-  gulp.watch(`${srcPath}**/*.pug`, build);
-  gulp.watch(`${srcPath}intermediate/**/*.njk`, compileNunjucksToMjml);
+  gulp.watch(`${srcPath}**/*.njk`, build);
   gulp.watch(`${srcPath}mjml/**/*.mjml`, compileMjmlToHtml);
-  gulp.watch(`${srcPath}images/**/*`, optimizeImages);
 }
 
 // Exporter les tâches
-exports.build = build;
-exports.watch = watchFiles;
-exports.default = build;
+export const buildTask = build;
+export const watchTask = watchFiles;
+export default build;
